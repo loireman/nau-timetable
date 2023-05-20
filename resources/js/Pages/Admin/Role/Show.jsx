9cd359be8033fba3cb4447120c95ca3f78163a5c
evlarionov <116222899+evlarionov@users.noreply.github.com>
2023-05-20 14:29:50 +0300

    show pages

--- resources/js/Pages/Admin/Role/Show.jsx
+++ resources/js/Pages/Admin/Role/Show.jsx
@@ -1,21 +1,33 @@
 import AuthenticatedLayout from '@/Layouts/Admin/AdminLayout';
 import { Head } from '@inertiajs/react';
 
-export default function Dashboard({ auth }) {
-    return (
-        <AuthenticatedLayout
-            user={auth.user}
-            header={<h2 className="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">Dashboard</h2>}
-        >
-            <Head title="Dashboard" />
+export default function Show({ auth, role, roleHasPermissions }) {
+  return (
+    <AuthenticatedLayout
+      user={auth.user}
+      header={<h2 className="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">Dashboard</h2>}
+    >
+      <Head title="Dashboard" />
 
-            <div className="py-12">
-                <div className="max-w-7xl mx-auto sm:px-6 lg:px-8">
-                    <div className="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
-                        <div className="p-6 text-gray-900 dark:text-gray-100">You're logged in admin!</div>
-                    </div>
-                </div>
+      <div className="py-12 ">
+        <div className="max-w-7xl mx-auto sm:px-6 lg:px-8">
+          <div className="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
+            <div className="p-6 text-gray-900 dark:text-gray-100">You're logged in admin!</div>
+
+            <div className="ml-8 dark:text-gray-100">
+            <span className="text-xl font-semibold ">Назва ролі: {role.name}</span>
+                <h3 className='mt-6'>Дозволи для ролі:</h3>
+                <ul>
+                    {roleHasPermissions.map((permission) => (
+                    <li key={permission.id}>{permission}</li>
+                    ))}
+                </ul>
             </div>
-        </AuthenticatedLayout>
-    );
+        </div>
+
+         
+        </div>
+      </div>
+    </AuthenticatedLayout>
+  );
 }
