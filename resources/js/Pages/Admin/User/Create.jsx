8c064ac88ca034ba909257e64fbf05df9efc1360
evlarionov <116222899+evlarionov@users.noreply.github.com>
2023-05-21 19:46:40 +0300

    create user (problem)

--- resources/js/Pages/Admin/User/Create.jsx
+++ resources/js/Pages/Admin/User/Create.jsx
@@ -1,18 +1,103 @@
-import AuthenticatedLayout from '@/Layouts/Admin/AdminLayout';
-import { Head } from '@inertiajs/react';
+import Checkbox from "@/Components/Checkbox";
+import InputError from "@/Components/InputError";
+import InputLabel from "@/Components/InputLabel";
+import PrimaryButton from "@/Components/PrimaryButton";
+import TextInput from "@/Components/TextInput";
+import AuthenticatedLayout from "@/Layouts/Admin/AdminLayout";
+import { Head, useForm } from "@inertiajs/react";
+import { useState } from "react";
 
-export default function Dashboard({ auth }) {
+export default function Create({ auth, roles}) {
+    const { data, setData, post, processing, errors, reset } = useForm({
+        name: "",
+        roles: [],
+    });
+
+    const handleCheckers = (event) => {
+        const value = parseInt(event.target.value);
+        if (event.target.checked) {
+            setData((prevItems) => ({
+                ...prevItems,
+                roles: [...prevItems.roles, value],
+            }));
+        } else {
+            setData((prevItems) => ({
+                ...prevItems,
+                roles: prevItems.roles.filter(
+                    (item) => item !== value
+                ),
+            }));
+        }
+    };
+
+    const submit = (e) => {
+        e.preventDefault();
+
+        post(route("user.store"));
+    };
+
     return (
         <AuthenticatedLayout
             user={auth.user}
-            header={<h2 className="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">Dashboard</h2>}
+            header={
+                <h2 className="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
+                    Створення ролі доступу
+                </h2>
+            }
         >
-            <Head title="Dashboard" />
+            <Head title="Створення ролі доступу" />
 
             <div className="py-12">
                 <div className="max-w-7xl mx-auto sm:px-6 lg:px-8">
-                    <div className="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
-                        <div className="p-6 text-gray-900 dark:text-gray-100">You're logged in admin!</div>
+                    <div className="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg p-4 text-gray-300">
+                        <span className="text-xl font-semibold">
+                            Створити роль доступу
+                        </span>
+                        <form className="px-4 py-8" onSubmit={submit}>
+                            <InputLabel htmlFor="name" value="Назва" />
+                            <TextInput
+                                id="name"
+                                name="name"
+                                value={data.name}
+                                className="mt-1 block w-full"
+                                autoComplete="name"
+                                isFocused={true}
+                                onChange={(e) =>
+                                    setData("name", e.target.value)
+                                }
+                                required
+                            />
+                            <InputError
+                                message={errors.name}
+                                className="mt-2"
+                            />
+                            <div className="block mt-4">
+                                {Object.entries(roles).map(
+                                    ([key, value]) => (
+                                        <label className="flex items-center">
+                                            <Checkbox
+                                                name={value}
+                                                value={key}
+                                                checked={data.roles.includes(
+                                                    parseInt(key)
+                                                )}
+                                                onChange={handleCheckers}
+                                            />
+                                            <span className="ml-2 text-sm text-gray-600 dark:text-gray-400">
+                                                {value}
+                                            </span>
+                                        </label>
+                                    )
+                                )}
+                            </div>
+
+                            <PrimaryButton
+                                className="mt-16 w-full grid"
+                                disabled={processing}
+                            >
+                                <span className="text-[1rem]">Створити</span>
+                            </PrimaryButton>
+                        </form>
                     </div>
                 </div>
             </div>
