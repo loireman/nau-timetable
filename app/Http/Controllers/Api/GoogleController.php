798af1ee0f259e5bd7f3e9c43af2d3440e65def1
loireman <akarvasarskij@gmail.com>
2024-02-26 10:22:57 +0100

    google auth 2

---
+++ app/Http/Controllers/Api/GoogleController.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Support\Facades\Auth;
+
+use App\Models\User;
+
+class GoogleController extends Controller
+{
+    private function getClient():\Google_Client
+    {
+        // load our config.json that contains our credentials for accessing google's api as a json string
+        $configJson = base_path().'/config.json';
+
+        // define an application name
+        $applicationName = 'myfancyapp';
+
+        // create the client
+        $client = new \Google_Client();
+        $client->setApplicationName($applicationName);
+        $client->setAuthConfig($configJson);
+        $client->setAccessType('offline'); // necessary for getting the refresh token
+        $client->setApprovalPrompt ('force'); // necessary for getting the refresh token
+        // scopes determine what google endpoints we can access. keep it simple for now.
+        $client->setScopes(
+            [
+                \Google\Service\Oauth2::USERINFO_PROFILE,
+                \Google\Service\Oauth2::USERINFO_EMAIL,
+                \Google\Service\Oauth2::OPENID,
+                \Google\Service\Drive::DRIVE_METADATA_READONLY // allows reading of google drive metadata
+            ]
+        );
+        $client->setIncludeGrantedScopes(true);
+        return $client;
+    }
+
+    public function getAuthUrl(Request $request):JsonResponse
+    {
+        /**
+         * Create google client
+         */
+        $client = $this->getClient();
+
+        /**
+         * Generate the url at google we redirect to
+         */
+        $authUrl = $client->createAuthUrl();
+
+        /**
+         * HTTP 200
+         */
+        return response()->json($authUrl, 200);
+    }
+
+    
+}
