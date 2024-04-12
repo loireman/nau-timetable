75313632f3bc93ccee7d90d022311ce27a5b9d56
loireman <akarvasarskij@gmail.com>
2024-04-12 23:46:53 +0200

    npm build

--- public/build/assets/PrimaryButton-2667c952.js
+++
@@ -1,1 +0,0 @@
-import{j as s}from"./app-de70be5c.js";function m({className:t="",disabled:r,children:o,...n}){return s.jsx("button",{...n,className:"button-primary "+t,disabled:r,children:o})}export{m as P};
