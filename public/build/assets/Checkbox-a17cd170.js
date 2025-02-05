f583effc92bb270f1180db3ce84945c073457f27
loireman <akarvasarskij@gmail.com>
2025-02-05 12:08:41 +0100

    npm build

--- public/build/assets/Checkbox-a17cd170.js
+++
@@ -1,1 +0,0 @@
-import{j as t}from"./app-abb73a69.js";function r({className:o="",...e}){return t.jsx("input",{...e,type:"checkbox",className:"form-checkbox "+o})}export{r as C};
---
+++ public/build/assets/PrimaryButton-b95c4174.js
@@ -0,0 +1,1 @@
+import{j as s}from"./app-d33b0834.js";function m({className:t="",disabled:r,children:o,...n}){return s.jsx("button",{...n,className:"button-primary "+t,disabled:r,children:o})}export{m as P};
