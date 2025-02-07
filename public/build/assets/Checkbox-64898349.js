1e1a90456d7e7b86c9d32ddc812196a94013349c
loireman <akarvasarskij@gmail.com>
2025-02-07 11:40:52 +0100

    npm build

--- public/build/assets/Checkbox-64898349.js
+++
@@ -1,1 +0,0 @@
-import{j as t}from"./app-d33b0834.js";function r({className:o="",...e}){return t.jsx("input",{...e,type:"checkbox",className:"form-checkbox "+o})}export{r as C};
---
+++ public/build/assets/InputError-42e959f7.js
@@ -0,0 +1,1 @@
+import{j as t}from"./app-3f991094.js";function e({message:r,className:n="",...o}){return r?t.jsx("p",{...o,className:"form-required "+n,children:r}):null}export{e as I};
