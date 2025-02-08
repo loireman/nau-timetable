aaedb01dc4566ca03cb9c42c1b778fad204c090b
loireman <akarvasarskij@gmail.com>
2025-02-08 22:49:21 +0100

    npm build

--- public/build/assets/InputError-2b419513.js
+++
@@ -1,1 +0,0 @@
-import{j as t}from"./app-e9dc3a82.js";function e({message:r,className:n="",...o}){return r?t.jsx("p",{...o,className:"form-required "+n,children:r}):null}export{e as I};
--- public/build/assets/PrimaryButton-ce25a64a.js
+++
@@ -1,1 +0,0 @@
-import{j as s}from"./app-e9dc3a82.js";function m({className:t="",disabled:r,children:o,...n}){return s.jsx("button",{...n,className:"button-primary "+t,disabled:r,children:o})}export{m as P};
