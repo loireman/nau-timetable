89035beec0347501a51732ee5bf64cce02e24d34
loireman <akarvasarskij@gmail.com>
2025-04-01 10:34:50 +0200

    1.1.5.2

--- app/Console/Commands/SendScheduledTimetableNotifications.php
+++ app/Console/Commands/SendScheduledTimetableNotifications.php
@@ -15,18 +15,18 @@ class SendScheduledTimetableNotifications extends Command
     protected $description = 'Sends scheduled timetable notifications to subscribed users.';
 
     protected $scheduledTimes = [
-        '05:55',
-        '07:45',
-        '09:35',
-        '11:25',
-        '13:15',
-        '15:05',
-        '16:55',
+        '07:55',
+        '09:45',
+        '11:35',
+        '13:25',
+        '15:15',
+        '16:05',
+        '18:55',
     ];
 
     public function handle()
     {
-        $now = Carbon::now();
+        $now = Carbon::now('Europe/Kyiv');
         $dayOfWeek = $now->dayOfWeek;
 
         if ($dayOfWeek == 0) {
@@ -35,22 +35,36 @@ class SendScheduledTimetableNotifications extends Command
         }
 
         $currentTime = $now->format('H:i');
+        
+        if (env('APP_ENV') == 'local') {
+            self::send();
+            $this->info("Timetable notifications sent for {$currentTime} (local).");
+        } else {
+            foreach ($this->scheduledTimes as $scheduledTime) {
+                $scheduled = $now->createFromFormat('H:i', $scheduledTime, 'Europe/Kyiv'); 
 
-        if (env('APP_ENV') == 'local' || in_array($currentTime, $this->scheduledTimes)) {
-            $subscribedUsers = TguserGroupRelation::where('subscription', true)
-                ->whereNotNull('telegram_id')
-                ->pluck('telegram_id');
-
-            foreach ($subscribedUsers as $telegramId) {                
-                $timetableMessage = Timetables::getCurrentPair($telegramId);
-
-                if ($timetableMessage[1]) {
-                    app(Nutgram::class)->sendMessage($timetableMessage[0], $telegramId, parse_mode: ParseMode::HTML);
+                if ($currentTime == $scheduled) {
+                    self::send();
+                    $this->info("Timetable notifications sent for {$currentTime} (prod).");
+                    return;
                 }
             }
-            $this->info("Timetable notifications sent for {$currentTime}.");
-        } else {
+
             $this->info("No notification scheduled for {$currentTime}.");
         }
+    }
+
+    private function send() {
+        $subscribedUsers = TguserGroupRelation::where('subscription', true)
+                ->whereNotNull('telegram_id')
+                ->pluck('telegram_id');
+                
+                foreach ($subscribedUsers as $telegramId) {                
+                    $timetableMessage = Timetables::getCurrentPair($telegramId);
+                    
+                    if ($timetableMessage[1]) {
+                        app(Nutgram::class)->sendMessage($timetableMessage[0], $telegramId, parse_mode: ParseMode::HTML);
+                    }
+                }
     }
 }
